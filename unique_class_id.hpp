9d5ee45d087e4279c5dcc21b4bfca9f889238b85
Yaroslav <elektroyar@yandex.ru>
2024-05-14 06:46:56 +0300

    added DEFINE_FINAL_UNIQUE_CLASS_ID

--- unique_class_id.hpp
+++ unique_class_id.hpp
@@ -6,36 +6,44 @@
 #include <cstdint>
 
 namespace unique_class_id {
-	using class_id_t = std::uint32_t;
+    using class_id_t = std::uint32_t;
 
-	namespace detail  {
-		class_id_t djb2_hash(const char *str) {
-			std::uint32_t hash	= 5381;
-			int c = 0;
-			while ((c = *str++)) {
-				hash = ((hash  << 5) + hash) + c;
-			}
-			return static_cast<class_id_t>(hash);
-		}
-	}
+    namespace detail  {
+        class_id_t djb2_hash(const char *str) {
+            std::uint32_t hash  = 5381;
+            int c = 0;
+            while ((c = *str++)) {
+                hash = ((hash  << 5) + hash) + c;
+            }
+            return static_cast<class_id_t>(hash);
+        }
+    }
 
-	template<class T>
-	class_id_t get_class_id() {
-		static const class_id_t id = detail::djb2_hash(typeid(T).name());
-		return id;
-	}
+    template<class T>
+    class_id_t get_class_id() {
+        static const class_id_t id = detail::djb2_hash(typeid(T).name());
+        return id;
+    }
 
-	template<class T1, class T2>
-	bool is_same_class(T2 *second) {
-		static const class_id_t id1 = get_class_id<T1>();
-		class_id_t id2 = second->id();
-		return id1 == id2;
-	}
+    template<class T1, class T2>
+    bool is_same_class(T2 *second) {
+        static const class_id_t id1 = get_class_id<T1>();
+        class_id_t id2 = second->id();
+        return id1 == id2;
+    }
 }
 
 #define DEFINE_UNIQUE_CLASS_ID(T)                       \
 public:                                                 \
 virtual unique_class_id::class_id_t id() const {        \
+    static const unique_class_id::class_id_t id =       \
+        unique_class_id::get_class_id<T>();             \
+    return id;                                          \
+}
+
+#define DEFINE_FINAL_UNIQUE_CLASS_ID(T)                 \
+public:                                                 \
+unique_class_id::class_id_t id() const override final { \
     static const unique_class_id::class_id_t id =       \
         unique_class_id::get_class_id<T>();             \
     return id;                                          \
